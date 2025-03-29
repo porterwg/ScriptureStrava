be5f15e8234e62cb01f86e587e0eb57ba6548109
Jordan Goddard <jordangoddard6@gmail.com>
2025-03-29 15:45:07 -0600

    Go navigation to and from PostStudy page working

--- my-app/src/components/types.ts
+++
@@ -1,6 +0,0 @@
-export interface Goal {
-  title: string;
-  subtitle: string;
-}
-
-export type TabType = "goals" | "posts";
--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 // Update App.js to include the new routes
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './Navbar';
-import UnjoinedGroup from './Group';
-import GroupsList from './groups';
-import PostsPage from './posts';
-import ProfilePage from './profile';
-import GospelLibrary from './gospelLibrary';
-import LoginPage from './LoginPage';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import UnjoinedGroup from "./Group";
+import GroupsList from "./groups";
+import PostsPage from "./posts";
+import ProfilePage from "./profile";
+import GospelLibrary from "./gospelLibrary";
+import LoginPage from "./LoginPage";
+import StudyPostPage from "./PostStudy";
+import PostStudy from "./PostStudy";
 
 function App() {
   return (
@@ -20,11 +22,11 @@ function App() {
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/" element={<PostsPage />} />
         <Route path="/gospelLibrary" element={<GospelLibrary />} />
-        <Route path="/login" element={<LoginPage />} /> 
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/poststudy" element={<PostStudy />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
